22c51828528b85e829dfcdf644304141d3e906b8
Cian <cian@ciangallagher.net>
2020-12-30 22:18:37 +0000

    CLI: Add func to format header styling.

--- pkg/printer/printer.go
+++ pkg/printer/printer.go
@@ -9,31 +9,31 @@ import (
 func NewTable(w io.Writer, headers []string) (t *tablewriter.Table) {
 	t = tablewriter.NewWriter(w)
 	t.SetHeader(headers)
-	applyStyle(t)
 
 	return
 }
 
+func HeaderStyle(t *tablewriter.Table) *tablewriter.Table {
+	t.SetHeaderColor(tablewriter.Colors{tablewriter.Bold},
+		tablewriter.Colors{tablewriter.Bold},
+		tablewriter.Colors{tablewriter.Bold},
+		tablewriter.Colors{tablewriter.Bold},
+		tablewriter.Colors{tablewriter.Bold})
+	return t
+}
+
 func SuccessStyle(t *tablewriter.Table, data []string) *tablewriter.Table {
-	t.Rich(data, []tablewriter.Colors{tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiGreenColor}, tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiGreenColor}})
+	t.Rich(data, []tablewriter.Colors{
+		tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiCyanColor},
+		tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiGreenColor},
+		tablewriter.Colors{tablewriter.Bold},
+		tablewriter.Colors{tablewriter.Bold},
+		tablewriter.Colors{tablewriter.Bold},
+	})
 	return t
 }
 
 func ErrorStyle(t *tablewriter.Table, data []string) *tablewriter.Table {
 	t.Rich(data, []tablewriter.Colors{tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiRedColor}, tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiRedColor}})
 	return t
-}
-
-func applyStyle(t *tablewriter.Table) {
-	t.SetAutoWrapText(false)
-	t.SetAutoFormatHeaders(true)
-	t.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
-	t.SetAlignment(tablewriter.ALIGN_LEFT)
-	t.SetCenterSeparator("")
-	t.SetColumnSeparator("")
-	t.SetRowSeparator("")
-	t.SetHeaderLine(false)
-	t.SetBorder(false)
-	t.SetTablePadding("\t")
-	t.SetNoWhiteSpace(true)
 }
