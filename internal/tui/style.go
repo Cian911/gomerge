7261bc383aef12f6a2b1e5c702751df82b78c01f
Cian Gallagher <cian.gallagher@zendesk.com>
2024-02-12 23:34:27 +0000

    table style

--- internal/tui/style.go
+++ internal/tui/style.go
@@ -6,74 +6,75 @@ import (
 )
 
 var (
-  mainViewStyle = lipgloss.NewStyle().
-	  BorderForeground(lipgloss.Color("240")).
-    BorderRight(true)
+	mainViewStyle = lipgloss.NewStyle().
+			BorderForeground(lipgloss.Color("240")).
+			BorderRight(true)
 
-  tableStyle = table.DefaultStyles().
-    Header.
-    BorderStyle(lipgloss.NormalBorder()).
-    BorderForeground(lipgloss.Color("240")).
-    BorderBottom(true).
-    Bold(true)
+	tableStyle = table.DefaultStyles().
+			Header.
+			BorderStyle(lipgloss.NormalBorder()).
+			BorderForeground(lipgloss.Color("240")).
+			Padding(1).
+			BorderBottom(true).
+			Bold(true)
 
-  tableSelectedStyle = table.DefaultStyles().
-    Selected.
-    Foreground(lipgloss.Color("#fff")).
-		Background(lipgloss.Color("57")).
-		Bold(true)   
+	tableSelectedStyle = table.DefaultStyles().
+				Selected.
+				Foreground(lipgloss.Color("#fff")).
+				Background(lipgloss.Color("57"))
 
-  tableCellStyle = table.DefaultStyles().
-    Cell.
-    Padding(1)
+	tableCellStyle = table.DefaultStyles().
+			Cell.
+			PaddingTop(2).
+			PaddingBottom(2)
 
 	detailViewStyle = lipgloss.NewStyle().
 			Padding(1, 1).
-      BorderLeft(true).
-      BorderRight(true).
-      BorderStyle(lipgloss.NormalBorder()).
-      BorderForeground(lipgloss.Color("63")).
-      BorderBottom(true).BorderBottomBackground(lipgloss.Color("#fff"))
+			BorderLeft(true).
+			BorderRight(true).
+			BorderStyle(lipgloss.NormalBorder()).
+			BorderForeground(lipgloss.Color("63")).
+			BorderBottom(true).BorderBottomBackground(lipgloss.Color("#fff"))
 
-  detailViewBranchStyle = lipgloss.NewStyle().
-    Italic(true).
-    Bold(true).
-    Foreground(lipgloss.Color("#CBCBCB")).
-    MarginLeft(3)
+	detailViewBranchStyle = lipgloss.NewStyle().
+				Italic(true).
+				Bold(true).
+				Foreground(lipgloss.Color("#CBCBCB")).
+				MarginLeft(3)
 
-  detailViewTitleStyle = lipgloss.NewStyle().
-    Bold(true).
-    Foreground(lipgloss.Color("#fff")).
-    MarginLeft(3).
-    Align(lipgloss.Left)
+	detailViewTitleStyle = lipgloss.NewStyle().
+				Bold(true).
+				Foreground(lipgloss.Color("#fff")).
+				MarginLeft(3).
+				Align(lipgloss.Left)
 
-  detailViewStateStyle = lipgloss.NewStyle().
-    Bold(true).
-    Foreground(lipgloss.Color("#fff")).
-    MarginLeft(3).
-    Align(lipgloss.Left)
+	detailViewStateStyle = lipgloss.NewStyle().
+				Bold(true).
+				Foreground(lipgloss.Color("#fff")).
+				MarginLeft(3).
+				Align(lipgloss.Left)
 
-  detailViewDescriptionStyle = lipgloss.NewStyle().
-    MarginLeft(3).
-    Foreground(lipgloss.Color("#fff"))
+	detailViewDescriptionStyle = lipgloss.NewStyle().
+					MarginLeft(3).
+					Foreground(lipgloss.Color("#fff"))
 
-  detailViewDefaultMsgStyle = lipgloss.NewStyle().
-    Align(lipgloss.Center).
-    Bold(true).
-    Foreground(lipgloss.Color("#fff"))
+	detailViewDefaultMsgStyle = lipgloss.NewStyle().
+					Align(lipgloss.Center).
+					Bold(true).
+					Foreground(lipgloss.Color("#fff"))
 
 	helpViewStyle = lipgloss.NewStyle().
 			Foreground(lipgloss.AdaptiveColor{Light: "#343433", Dark: "#C1C6B2"}).
 			Background(lipgloss.AdaptiveColor{Light: "#24273a", Dark: "#181926"}).
 			Align(lipgloss.Center)
 
-  mergeGlyph = ""
-  successGlyph = ""
-  errorGlyph = "󰅙"
-  waitingGlyph = "󰗨"
-  prGlyph = ""
-  timeGlyph = ""
-  changeReqeuestGlyp = "󰌑"
-  authorGlyph =""
-  repoGlyph = ""
+	mergeGlyph         = ""
+	successGlyph       = ""
+	errorGlyph         = "󰅙"
+	waitingGlyph       = "󰗨"
+	prGlyph            = ""
+	timeGlyph          = ""
+	changeReqeuestGlyp = "󰌑"
+	authorGlyph        = ""
+	repoGlyph          = ""
 )
--- pkg/cli/list/list.go
+++ pkg/cli/list/list.go
@@ -4,42 +4,43 @@ import (
 	"log"
 
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/cian911/go-merge/internal/tui"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+
+	"github.com/cian911/go-merge/internal/tui"
 )
 
 func List() (c *cobra.Command) {
-  c = &cobra.Command{
-    Use: "list",
-    Short: "List all pull requests for a repo you want to action.",
-    Run: func(cmd *cobra.Command, args []string) {
-      if viper.ConfigFileUsed() != "" {
-        // Use the config
-      } else {
-        f, err := tea.LogToFile("debug.log", "debug")
-        if err != nil {
-          log.Fatalf("%v", err)
-        }
-        defer f.Close()
+	c = &cobra.Command{
+		Use:   "list",
+		Short: "List all pull requests for a repo you want to action.",
+		Run: func(cmd *cobra.Command, args []string) {
+			if viper.ConfigFileUsed() != "" {
+				// Use the config
+			} else {
+				f, err := tea.LogToFile("debug.log", "debug")
+				if err != nil {
+					log.Fatalf("%v", err)
+				}
+				defer f.Close()
 
-        // Use the CLI args
-        model, err := tui.New()
-        model.Init()
-        
-        if err != nil {
-          log.Fatal(err)
-        }
+				// Use the CLI args
+				model, err := tui.New()
+				model.Init()
 
-        p := tea.NewProgram(model, tea.WithAltScreen(), tea.WithMouseCellMotion())
-        if err := p.Start(); err != nil {
-          log.Fatalf("Could not start tui: %v", err)
-        }
-      }
-    },
-  }
-  
-  return
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				p := tea.NewProgram(model, tea.WithAltScreen(), tea.WithMouseCellMotion())
+				if err := p.Start(); err != nil {
+					log.Fatalf("Could not start tui: %v", err)
+				}
+			}
+		},
+	}
+
+	return
 }
 
 func initConfig() {}
